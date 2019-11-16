8f69b2382b4cc68f795f543bd30dca57ec16d5b3
igranic8720 <55810432+igranic8720@users.noreply.github.com>
2019-11-16 16:17:05 -0500

    Good commit message

---
+++ Transport Management System WPF/Transport Management System WPF/BuyerClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transport_Management_System_WPF
+{
+    class BuyerClass
+    {
+        public static void ParseContracts()
+        {
+
+        } 
+    }
+}
