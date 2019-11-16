109b3e2298cd746555555c81d40ac8e8281eaa92
DuaneCressman <49034609+DuaneCressman@users.noreply.github.com>
2019-11-16 14:34:27 -0500

    Create MockupCMP.cs

---
+++ SoftQualv1/SoftQualv1/MockupCMP.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftQualv1
+{
+    class MockupCMP
+    {
+        string[] a = {"|Hardware Depot|0|0|Belleville|Windsor|0|",
+                      "Atlantis Railway|1|3|Hamilton|Oshawa|0|",
+                      "Big Oil|0|0|Windsor|Hamilton|1|",
+                      "Rockomax Engineering|1|10|Belleville|Toronto|1|",
+                      "Space J|0|0|Toronto|Belleville|1|",
+                      "Rockomax Engineering|0|0|Belleville|Ottawa|1|",
+                      "Tom Hortons|0|0|Ottawa|Toronto|0|",
+                      "Sushi Noodle|0|0|Toronto|Ottawa|0",
+                      "Atlantis|Railway|0|0|Kingston|Windsor|0|"
+                      };
+
+    }
+}
