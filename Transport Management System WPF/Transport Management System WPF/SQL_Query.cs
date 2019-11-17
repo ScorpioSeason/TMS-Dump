a9cd438e3e7aeb8572d56d5d1ec8b8bcbbd6ef0e
DuaneCressman <49034609+DuaneCressman@users.noreply.github.com>
2019-11-16 21:26:35 -0500

    commit duanes ssiza

--- Transport Management System WPF/Transport Management System WPF/SQL_Query.cs
+++ Transport Management System WPF/Transport Management System WPF/SQL_Query.cs
@@ -4,10 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
-
-using System;
 using System.Data;
-
 using MySql.Data;
 using MySql.Data.MySqlClient;
 
