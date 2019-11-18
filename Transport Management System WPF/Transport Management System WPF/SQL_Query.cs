449c2a6548827b9a7d54520d2fbdf0771dd40ae6
z <Zena@z.local>
2019-11-18 15:07:58 -0500

    Zena fixing Zena's mistakes +Commenting like a boss for the SQL connection

--- Transport Management System WPF/Transport Management System WPF/SQL_Query.cs
+++ Transport Management System WPF/Transport Management System WPF/SQL_Query.cs
@@ -12,66 +12,44 @@ using System.Diagnostics;
 
 namespace Transport_Management_System_WPF
 {
-    static class SQL_Query
+    class SQL_Query
     {
-        public static string sql = null;
-        public static List<string> sqlReads = null; 
-        public static void ContractCalling()
+        public static List<string> sqlReads = null;     //List to hold returns 
+
+        public static void ContractCalling()            //This is contract calling from the contract marketplace
         {
-            try
+            MySqlConnection connection = new MySqlConnection("database = 'cmd'; server = 159.89.117.198; port = 3306; user id = 'DevOSHT'; Password = 'Snodgr4ss!;'");
+
+            connection.CommandType = System.Data.CommandType.Text;  //I want my connection string to be text
+            
+            connection.CommandText = "SELECT * From Contract";      //Query text string 
+
+            MySqlCommand command = connection.CreateCommand();      //Create the mySQL command
+
+            try                         //"Try" :p
             {
-                MySqlConnection con = new MySqlConnection("database = c,d; server = 159.89.117.198; port = 3306; user id = DevOSHT; Password = Snodgr4ss!;");
-                
-
-                con.Open();
-                MySqlCommand com = con.CreateCommand();
-
-                com.CommandType = System.Data.CommandType.Text;
-                com.CommandText = "SELECT * From Contract";
-                MySql.Data.MySqlClient.MySqlDataReader reader = com.ExecuteReader();
-
-
-                if (reader.HasRows)
+                connection.Open();      //Open Connection
+                MySqlDataReader reader = command.ExecuteReader();   //Call reader
+             
+                while (reader.Read())   //While there is something to read
                 {
-                    while (reader.Read())
-                    {
-                        sqlReads.Add(reader.GetString(0));
-                        sqlReads.Add(reader.GetString(1));
-                        sqlReads.Add(reader.GetString(2));
-                        sqlReads.Add(reader.GetString(3));
-                        sqlReads.Add(reader.GetString(4));
-                        sqlReads.Add(reader.GetString(5));
-                    }
-                    reader.Close();
+                    sqlReads.Add(reader.GetString(0));
+                    sqlReads.Add(reader.GetString(1));
+                    sqlReads.Add(reader.GetString(2));
+                    sqlReads.Add(reader.GetString(3));
+                    sqlReads.Add(reader.GetString(4));
+                    sqlReads.Add(reader.GetString(5));
                 }
+                reader.Close();     //Close the reader - This means you need to properly open the reader duuuhhhhhhh
+                
+                connection.Close();     //Close Connection
             }
-            catch (Exception ex)
+            catch (Exception ex)        //Exception: Gross!
             {
                 //MessageBox.Show(ex.Message);
+                Console.WriteLine(ex.Message);  //Log exception
             }
-            //sql = null;
-            //string connectionString = null;
-            //SqlConnection connection;
-            //SqlDataAdapter adapter = new SqlDataAdapter();
-            //connectionString = "server=159.89.117.198,3306;database=cmp;uid=DevOSHT;pwd=Snodgr4ss!;";
-            //connection = new SqlConnection(connectionString);
-            //sql = "SELECT * FROM Contract;";
-
-            //connection.Open();
-            ////adapter.InsertCommand = new SqlCommand(sql, connection);
-            ////adapter.InsertCommand.ExecuteNonQuery();
-
-            //SqlCommand cmd = new SqlCommand(sql, connection);
-            //using (SqlDataReader reader = cmd.ExecuteReader())
-            //{
-            //    while (reader.Read())
-            //    {
-            //        string readString = reader.ToString().Trim();
-            //        sqlReads.Add(readString);
-            //    }
-            //}
-            //connection.Close();
-            //return;
         }
     }
 }
+
