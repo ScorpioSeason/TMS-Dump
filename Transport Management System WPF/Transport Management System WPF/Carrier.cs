108d6e718a2ef7df1bca3645acac83dd388463dc
igranic8720 <55810432+igranic8720@users.noreply.github.com>
2019-11-19 19:18:56 -0500

    set up classes for writing... functional but ust for adding classes to work on. merge after.

---
+++ Transport Management System WPF/Transport Management System WPF/Carrier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transport_Management_System_WPF
+{
+    class Carrier
+    {
+        public string Carrier_Name { get; set; }
+        public string Depot_City { get; set; }
+        public float FTL_Rate { get; set; }
+        public float LTL_Rate { get; set; }
+        public float Reefer_Charge { get; set; }
+    }
+}
