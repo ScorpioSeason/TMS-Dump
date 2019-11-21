383725df6232c8e154462adfb9a75ffa5f1d6d17
Mbradshaw4861 <41707698+Mbradshaw4861@users.noreply.github.com>
2019-11-20 19:27:57 -0500

    Addition of Doxygen structured sample comments to Admin class

--- Transport Management System WPF/Transport Management System WPF/Admin.cs
+++ Transport Management System WPF/Transport Management System WPF/Admin.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace Transport_Management_System_WPF
 {
+
+    /* CLASS / FILE HEADER COMMENT: =================================================================================
+    *
+    *  \file		Shape.h
+    *  \ingroup		PROG 1385 - Assign 04: Shapes
+    *  \date		November 20, 2019
+    *  \author		8000 Cigarettes
+    *  \brief		This file contains library inclusions, global constants, and the Shape class definition.	  
+    *  \see			Files that this depends on		-- http://doxygen.nl/manual/commands.html#cmdref															  */
+    /**\class		Shape
+    *  \brief		Class brief description.
+    *  \details		This base class represents a simple shape, its attributes, and its functions. The shape has a 
+    *				name, a colour, and (virtual) perimeter, area, and overall dimension. The functions of the
+    *				shape involve creation, deletion, access, and manipulation of a shape; its colour and name. 
+    *
+    * =========================================================================================================== */
+
     class Admin
     {
+
+
+
+        // COP-OUT METHOD HEADER COMMENT -------------------------------------------------------------------------------
+        /**
+        *	\fn		    int Square()
+        *	\brief		To create a new Square by validating or else defaulting new values
+        *	\details	THis is if you have more to say about what the function does and don't want to inline comment
+        *	\param[in]	char[]	newColour		An incoming value meant to become the square's colour
+        *	\param[out]	char[]	newSideLength	An incoming value meant to become the square's side length
+        *	\exception	This is if we have some big ol try catches?
+        *	\see		CallsMade()
+        *	\return		None
+        *
+        * ---------------------------------------------------------------------------------------------------- */
+
     }
 }
