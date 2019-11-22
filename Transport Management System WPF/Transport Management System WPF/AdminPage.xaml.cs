f1a2903e2e5ecd981b6c1e5d3e2b3e25a6be2f78
Mbradshaw4861 <41707698+Mbradshaw4861@users.noreply.github.com>
2019-11-22 09:54:51 -0500

    Migration of Admin Pages from other Solution

--- Transport Management System WPF/Transport Management System WPF/AdminPage.xaml.cs
+++ Transport Management System WPF/Transport Management System WPF/AdminPage.xaml.cs
@@ -14,15 +14,76 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Transport_Management_System_WPF
+namespace SampleWFPUsingPages
 {
     /// <summary>
-    /// Interaction logic for Page1.xaml
+    /// Interaction logic for AdminPage.xaml
     /// </summary>
     public partial class AdminPage : Page
     {
+        List<TMSLog> searchResults = new List<TMSLog>();
+
         public AdminPage()
         {
-
+            InitializeComponent();
+            LogsList.ItemsSource = searchResults;
         }
+
+        //public AdminPage(object data) : this()
+        //{
+        //    // Bind to expense report data.
+        //    this.DataContext = data;
+        //}
+
+        // This adds logs which match search strings to the displayed list
+        private void LoadClick(object sender, RoutedEventArgs e)
+        {
+            // This is just test logs.
+            if (TMSLogger.ReadExistingLogFile())
+            {
+                searchResults.Clear();
+            }
+
+            // This actually populates the logs in the UI
+            //if ()
+            //{
+                // Search by time/date is currently incomplete
+            //}
+            
+            if (searchTags.Text.Trim() != "")
+            {
+                foreach (TMSLog l in TMSLogger.logs)
+                {
+                    if ((l.logType).Contains(searchTags.Text) || (l.logMessage).Contains(searchTags.Text))
+                    {
+                        searchResults.Add(l);
+                    }
+                    else if ((l.logClass).Contains(searchTags.Text) || (l.logMethod).Contains(searchTags.Text))
+                    {
+                        searchResults.Add(l);
+                    }
+                }
+            }
+            else
+            {
+                foreach (TMSLog l in TMSLogger.logs)
+                {
+                    searchResults.Add(l);
+                }
+            }
+
+            LogsList.Items.Refresh();
+        }
+
+        // This navigates to a new page where the details of the selected log are listed
+        private void ViewMoreClick(object sender, RoutedEventArgs e)
+        {
+            if (LogsList.SelectedItem != null)
+            {
+                ViewLogDetailsPage newpage = new ViewLogDetailsPage(this.LogsList.SelectedItem);
+                this.NavigationService.Navigate(newpage);
+            }
+        }
+
     }
 }
