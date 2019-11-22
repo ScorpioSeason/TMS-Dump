c4f0826e30e2e7f24bbec22a2ac9af6c3396d0dc
Mbradshaw4861 <41707698+Mbradshaw4861@users.noreply.github.com>
2019-11-22 10:42:02 -0500

    Update UnitTest1.cs
    Addition of doxygen file and class headers (unfilled)

--- Transport Management System WPF/UnitTestTMS/UnitTest1.cs
+++ Transport Management System WPF/UnitTestTMS/UnitTest1.cs
@@ -1,16 +1,36 @@
-﻿using System;
+﻿// ADMIN FILE HEADER COMMENT: =================================================================================
+/**
+ *  \file		Admin.cs
+ *  \ingroup	TMS
+ *  \date		November 20, 2019
+ *  \author		8000 Cigarettes - Megan
+ *  \brief	    This file contains the admin functionality	  
+ *  \see		MainWindow.xaml
+ *  \details    This file holds the functionality of the Admin class. The Admin has the ability to view logs as 
+ *              specified by time period, view details of specific logs, alter where the log files are stored, 
+ *              initiate backups of the TMS database, choose where the TMS db is backed up to, alter the Carrier 
+ *              Data Table, the Route Table, and the Rate / Fee Tables.                                       
+ *
+ * =========================================================================================================== */
+
+using System;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Transport_Management_System_WPF;
 
-
 namespace UnitTestTMS
-{
+{   
+    // CLASS HEADER COMMENT -----------------------------------------------------------------------------------
+    /**   
+    *   \class		Admin
+    *   \brief		This class runs the Admin UI functionality
+    *   \details	... static class?  
+    *   
+    * -------------------------------------------------------------------------------------------------------- */
     [TestClass]
     public class UnitTest1
     {
 
-        
         //Test city 7 - 0, FTL
         [TestMethod]
         public void routCalc1()
