313350dd8e1f982f6d76dd27076c2ecd67e062f7
Mbradshaw4861 <41707698+Mbradshaw4861@users.noreply.github.com>
2019-11-22 12:06:09 -0500

    Migration of Admin files (again)

--- Transport Management System WPF/TMSwPages/AdminClasses.cs
+++ Transport Management System WPF/TMSwPages/AdminClasses.cs
@@ -22,7 +22,8 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Documents;
 
-namespace SampleWFPUsingPages
+namespace Transport_Management_System_WPF
+//namespace SampleWFPUsingPages
 {
     // CLASS HEADER COMMENT -----------------------------------------------------------------------------------
     /**   
@@ -39,6 +40,18 @@ namespace SampleWFPUsingPages
         BackupTMS adminBackup = null;
         AlterTables adminAlter = null;
 
+        // COP-OUT METHOD HEADER COMMENT -------------------------------------------------------------------------------
+        /**
+        *	\fn			int Square()
+        *	\brief		To create a new Square by validating or else defaulting new values
+        *	\details	THis is if you have more to say about what the function does and don't want to inline comment
+        *	\param[in]	char[]	newColour		An incoming value meant to become the square's colour
+        *	\param[out]	char[]	newSideLength	An incoming value meant to become the square's side length
+        *	\exception	This is if we have some big ol try catches?
+        *	\see		CallsMade()
+        *	\return		None
+        *
+        * ---------------------------------------------------------------------------------------------------- */
         Admin()
         {
             //adminLogger = new TMSLogger();
@@ -56,12 +69,31 @@ namespace SampleWFPUsingPages
         // Data Table, the Route Table, and the Rate / Fee Tables.
     }
 
+    // CLASS HEADER COMMENT -----------------------------------------------------------------------------------
+    /**   
+    *   \class		Admin
+    *   \brief		This class runs the Admin UI functionality
+    *   \details	... static class?  
+    *   
+    * -------------------------------------------------------------------------------------------------------- */
     static public class TMSLogger
     {
         static string LoggerPath { set; get; }                                          // Stored location of the log file
         static public List<TMSLog> logs = new List<TMSLog>();
 
         // Add to internal Log list AND append to the external file
+        // COP-OUT METHOD HEADER COMMENT -------------------------------------------------------------------------------
+        /**
+        *	\fn			int Square()
+        *	\brief		To create a new Square by validating or else defaulting new values
+        *	\details	THis is if you have more to say about what the function does and don't want to inline comment
+        *	\param[in]	char[]	newColour		An incoming value meant to become the square's colour
+        *	\param[out]	char[]	newSideLength	An incoming value meant to become the square's side length
+        *	\exception	This is if we have some big ol try catches?
+        *	\see		CallsMade()
+        *	\return		None
+        *
+        * ---------------------------------------------------------------------------------------------------- */
         static public void LogIt(string newLogString)
         {
             TMSLog myLog = new TMSLog(newLogString);
@@ -71,6 +103,18 @@ namespace SampleWFPUsingPages
         }
 
         // Only add to the internal log list (i.e. read in from file)
+        // COP-OUT METHOD HEADER COMMENT -------------------------------------------------------------------------------
+        /**
+        *	\fn			int Square()
+        *	\brief		To create a new Square by validating or else defaulting new values
+        *	\details	THis is if you have more to say about what the function does and don't want to inline comment
+        *	\param[in]	char[]	newColour		An incoming value meant to become the square's colour
+        *	\param[out]	char[]	newSideLength	An incoming value meant to become the square's side length
+        *	\exception	This is if we have some big ol try catches?
+        *	\see		CallsMade()
+        *	\return		None
+        *
+        * ---------------------------------------------------------------------------------------------------- */
         static public void NewLog(string newLogString)
         {
             TMSLog myLog = new TMSLog(newLogString);
@@ -78,6 +122,18 @@ namespace SampleWFPUsingPages
         }
 
         // Read logs in from a file
+        // COP-OUT METHOD HEADER COMMENT -------------------------------------------------------------------------------
+        /**
+        *	\fn			int Square()
+        *	\brief		To create a new Square by validating or else defaulting new values
+        *	\details	THis is if you have more to say about what the function does and don't want to inline comment
+        *	\param[in]	char[]	newColour		An incoming value meant to become the square's colour
+        *	\param[out]	char[]	newSideLength	An incoming value meant to become the square's side length
+        *	\exception	This is if we have some big ol try catches?
+        *	\see		CallsMade()
+        *	\return		None
+        *
+        * ---------------------------------------------------------------------------------------------------- */
         static public bool ReadExistingLogFile()
         {
             bool readSuccess = true;
@@ -117,6 +173,18 @@ namespace SampleWFPUsingPages
 
         }
 
+        // COP-OUT METHOD HEADER COMMENT -------------------------------------------------------------------------------
+        /**
+        *	\fn			int Square()
+        *	\brief		To create a new Square by validating or else defaulting new values
+        *	\details	THis is if you have more to say about what the function does and don't want to inline comment
+        *	\param[in]	char[]	newColour		An incoming value meant to become the square's colour
+        *	\param[out]	char[]	newSideLength	An incoming value meant to become the square's side length
+        *	\exception	This is if we have some big ol try catches?
+        *	\see		CallsMade()
+        *	\return		None
+        *
+        * ---------------------------------------------------------------------------------------------------- */
         static public bool AppendLogFile(TMSLog newLog)
         {
             bool appendSuccess = true; 
@@ -154,6 +222,18 @@ namespace SampleWFPUsingPages
 
         // Move logs
 
+        // COP-OUT METHOD HEADER COMMENT -------------------------------------------------------------------------------
+        /**
+        *	\fn			int Square()
+        *	\brief		To create a new Square by validating or else defaulting new values
+        *	\details	THis is if you have more to say about what the function does and don't want to inline comment
+        *	\param[in]	char[]	newColour		An incoming value meant to become the square's colour
+        *	\param[out]	char[]	newSideLength	An incoming value meant to become the square's side length
+        *	\exception	This is if we have some big ol try catches?
+        *	\see		CallsMade()
+        *	\return		None
+        *
+        * ---------------------------------------------------------------------------------------------------- */
         static void ChangeLogLocation()
         {
             // Select a new location from the popup box
@@ -195,6 +275,13 @@ namespace SampleWFPUsingPages
 
     }
 
+    // CLASS HEADER COMMENT -----------------------------------------------------------------------------------
+    /**   
+    *   \class		Admin
+    *   \brief		This class runs the Admin UI functionality
+    *   \details	... static class?  
+    *   
+    * -------------------------------------------------------------------------------------------------------- */
     public class TMSLog
     {
         // Data for each individual log (not exactly protected)
@@ -206,6 +293,18 @@ namespace SampleWFPUsingPages
         public string BSV { set; get; } // Bar separated values
         public DateTime logTime { set; get; }
 
+        // COP-OUT METHOD HEADER COMMENT -------------------------------------------------------------------------------
+        /**
+        *	\fn			int Square()
+        *	\brief		To create a new Square by validating or else defaulting new values
+        *	\details	THis is if you have more to say about what the function does and don't want to inline comment
+        *	\param[in]	char[]	newColour		An incoming value meant to become the square's colour
+        *	\param[out]	char[]	newSideLength	An incoming value meant to become the square's side length
+        *	\exception	This is if we have some big ol try catches?
+        *	\see		CallsMade()
+        *	\return		None
+        *
+        * ---------------------------------------------------------------------------------------------------- */
         public TMSLog(string nUnparsed)
         {
             // Used in Logger / LogIt call
@@ -247,6 +346,13 @@ namespace SampleWFPUsingPages
 
     }
 
+    // CLASS HEADER COMMENT -----------------------------------------------------------------------------------
+    /**   
+    *   \class		Admin
+    *   \brief		This class runs the Admin UI functionality
+    *   \details	... static class?  
+    *   
+    * -------------------------------------------------------------------------------------------------------- */
     class BackupTMS
     {
         // Read store location
@@ -260,6 +366,13 @@ namespace SampleWFPUsingPages
         // If successful write, delete old copy of backup (do not immediately overwrite)
     }
 
+    // CLASS HEADER COMMENT -----------------------------------------------------------------------------------
+    /**   
+    *   \class		Admin
+    *   \brief		This class runs the Admin UI functionality
+    *   \details	... static class?  
+    *   
+    * -------------------------------------------------------------------------------------------------------- */
     class AlterTables
     {
         // Access tables
--- Transport Management System WPF/TMSwPages/ViewLogDetailsPage.xaml.cs
+++ Transport Management System WPF/TMSwPages/ViewLogDetailsPage.xaml.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿// ADMIN FILE HEADER COMMENT: =================================================================================
+/**
+ *  \file		Admin.cs
+ *  \ingroup	TMS
+ *  \date		November 20, 2019
+ *  \author		8000 Cigarettes - Megan
+ *  \brief	    This file contains the admin functionality	  
+ *  \see		MainWindow.xaml
+ *  \details    This file holds the functionality of the Admin class. The Admin has the ability to view logs as 
+ *              specified by time period, view details of specific logs, alter where the log files are stored, 
+ *              initiate backups of the TMS database, choose where the TMS db is backed up to, alter the Carrier 
+ *              Data Table, the Route Table, and the Rate / Fee Tables.                                       
+ *
+ * =========================================================================================================== */
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,15 +31,44 @@ using System.Windows.Shapes;
 
 namespace SampleWFPUsingPages
 {
-    /// <summary>
-    /// Interaction logic for ViewLogDetailsPage.xaml
-    /// </summary>
+    // CLASS HEADER COMMENT -----------------------------------------------------------------------------------
+    /**   
+    *   \class		Admin
+    *   \brief		This class runs the Admin UI functionality
+    *   \details	... static class?  
+    *   
+    * -------------------------------------------------------------------------------------------------------- */
     public partial class ViewLogDetailsPage : Page
     {
+        // COP-OUT METHOD HEADER COMMENT -------------------------------------------------------------------------------
+        /**
+        *	\fn			int Square()
+        *	\brief		To create a new Square by validating or else defaulting new values
+        *	\details	THis is if you have more to say about what the function does and don't want to inline comment
+        *	\param[in]	char[]	newColour		An incoming value meant to become the square's colour
+        *	\param[out]	char[]	newSideLength	An incoming value meant to become the square's side length
+        *	\exception	This is if we have some big ol try catches?
+        *	\see		CallsMade()
+        *	\return		None
+        *
+        * ---------------------------------------------------------------------------------------------------- */
         public ViewLogDetailsPage()
         {
             InitializeComponent();
         }
+
+        // COP-OUT METHOD HEADER COMMENT -------------------------------------------------------------------------------
+        /**
+        *	\fn			int Square()
+        *	\brief		To create a new Square by validating or else defaulting new values
+        *	\details	THis is if you have more to say about what the function does and don't want to inline comment
+        *	\param[in]	char[]	newColour		An incoming value meant to become the square's colour
+        *	\param[out]	char[]	newSideLength	An incoming value meant to become the square's side length
+        *	\exception	This is if we have some big ol try catches?
+        *	\see		CallsMade()
+        *	\return		None
+        *
+        * ---------------------------------------------------------------------------------------------------- */
         public ViewLogDetailsPage(object data) : this()
         {
             // Bind to incoming log data.
